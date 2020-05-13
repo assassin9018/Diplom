923c39814cf97278ed6cd4f0c8c36a428c00fc8e
assassin9018 <34675794+assassin9018@users.noreply.github.com>
2020-05-13 19:05:07 +0300

    Добавлен класс пользователя.

---
+++ PersonnelDepartment/DTO/User.cs
@@ -0,0 +1,29 @@
+﻿namespace PersonnelDepartment.DTO
+{
+    /// <summary>
+    /// Информация о пользователе
+    /// </summary>
+    internal class User
+    {
+        /// <summary>
+        /// Id записи в БД.
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// Логин пользователя
+        /// </summary>
+        public string Login { get; set; }
+        /// <summary>
+        /// Пароль пользователя
+        /// </summary>
+        public string Password { get; set; }
+        /// <summary>
+        /// Id личной карточки
+        /// </summary>
+        public string EmployeeId { get; set; }
+        /// <summary>
+        /// Сокращенное имя пользователя.
+        /// </summary>
+        public string ShortName { get; set; }
+    }
+}
