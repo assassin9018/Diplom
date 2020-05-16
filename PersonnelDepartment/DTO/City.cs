00c00be944ae514293b40bb0a7c9ef6c79c379a6
assassin9018 <34675794+assassin9018@users.noreply.github.com>
2020-05-16 20:07:14 +0300

    Добавил DTO для города и поправил DTO организации

---
+++ PersonnelDepartment/DTO/City.cs
@@ -0,0 +1,15 @@
+﻿namespace PersonnelDepartment.DTO
+{
+    class City
+    {
+        /// <summary>
+        /// Id записи в БД.
+        /// </summary>
+        public int Id { get; }
+
+        /// <summary>
+        /// Название города
+        /// </summary>
+        public string Name { get; }
+    }
+}
--- PersonnelDepartment/DTO/Organization.cs
+++ PersonnelDepartment/DTO/Organization.cs
@@ -9,18 +9,17 @@
         /// Id записи в БД.
         /// </summary>
         public int Id { get; }
+
         /// <summary>
         /// Наименование организации
         /// </summary>
         public string Name { get; }
+
         /// <summary>
-        /// Id города
+        /// Город
         /// </summary>
-        public int CityId { get; }
-        /// <summary>
-        /// Наименование города
-        /// </summary>
-        public string City { get; }
+        public City City { get; }
+
         /// <summary>
         /// Адрес организации
         /// </summary>
