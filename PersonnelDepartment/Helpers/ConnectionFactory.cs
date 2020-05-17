b6598778908311b97fc37601ad815d54cd3dba26
assassin9018 <34675794+assassin9018@users.noreply.github.com>
2020-05-17 15:10:54 +0300

    Добавил фабрику для подключения к БД

---
+++ PersonnelDepartment/Helpers/ConnectionFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace PersonnelDepartment.Helpers
+{
+    internal static class ConnectionFactory
+    {
+        private static string _connString = string.Empty;
+
+        public static SqlConnection GetSqlConnection()
+        {
+            if(string.IsNullOrEmpty(_connString))
+                throw new InvalidOperationException(RuStrings.ConnStringNotSet);
+
+            var conn = new SqlConnection(_connString);
+            conn.Open();
+            return conn;
+        }
+
+        public static void SetConnectionString(string connectionString)
+        {
+            connectionString = connectionString.Trim();
+            if(string.IsNullOrEmpty(connectionString))
+                throw new ArgumentException(RuStrings.EmptyConnString);
+            _connString = connectionString;
+        }
+    }
+}
