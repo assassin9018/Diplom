9482c64ede536588c1c496f8610c9044e4b34224
assassin9018 <34675794+assassin9018@users.noreply.github.com>
2020-05-17 15:50:23 +0300

    Добавил формы

---
+++ PersonnelDepartment/Windows/EducationWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PersonnelDepartment.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для EducationWindow.xaml
+    /// </summary>
+    public partial class EducationWindow : Window
+    {
+        public EducationWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Enter_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddCityBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddEduTypeBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddSpecialtyBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ PersonnelDepartment/Windows/SpecialtyWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PersonnelDepartment.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для SpecialtyWindow.xaml
+    /// </summary>
+    public partial class SpecialtyWindow : Window
+    {
+        public SpecialtyWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Enter_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
