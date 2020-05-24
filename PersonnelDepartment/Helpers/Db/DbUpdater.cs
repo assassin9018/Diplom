edf3a41570ea9d65b0a6f287329cea4468f836f7
assassin9018 <34675794+assassin9018@users.noreply.github.com>
2020-05-24 20:59:05 +0300

    Revert "Поправил баг"
    This reverts commit a96bfe4814b88b2f1d3a0bf9ca360ed7c5d8e9b1.

--- PersonnelDepartment/Helpers/Db/DbUpdater.cs
+++ PersonnelDepartment/Helpers/Db/DbUpdater.cs
@@ -127,8 +127,7 @@ namespace PersonnelDepartment.Helpers.Db
                 command.CommandType = CommandType.StoredProcedure;
                 command.Parameters.Add(new SqlParameter("@UsId", obj.Id));
                 command.Parameters.Add(new SqlParameter("@Login", obj.Login));
-                string password = AutorizationHelper.GetMd5Hash(obj.Password);
-                command.Parameters.Add(new SqlParameter("@Password", password));
+                command.Parameters.Add(new SqlParameter("@Password", obj.Password));
                 command.Parameters.Add(new SqlParameter("@Permissions", obj.Permissions));
                 command.Parameters.Add(new SqlParameter("@EmId", obj.Employee.Id));
 
