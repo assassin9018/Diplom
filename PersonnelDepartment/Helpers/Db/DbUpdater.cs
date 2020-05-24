65bd29a5e6fcb076a291ff39e28bbd2896001b83
assassin9018 <34675794+assassin9018@users.noreply.github.com>
2020-05-24 15:15:47 +0300

    Добавил методы обновления работников и пользователей

--- PersonnelDepartment/Helpers/Db/DbUpdater.cs
+++ PersonnelDepartment/Helpers/Db/DbUpdater.cs
@@ -53,5 +53,56 @@ namespace PersonnelDepartment.Helpers.Db
                     MessageBox.Show(RuStrings.RemoveNotExecuted);
             }
         }
+
+        public static void UpdateEmployee(EmployeeExtended obj)
+        {
+            const string sqlExpression = "UpdateEmployee";
+
+            using(SqlConnection connection = ConnectionFactory.GetSqlConnection())
+            {
+                SqlCommand command = new SqlCommand(sqlExpression, connection);
+                command.CommandType = CommandType.StoredProcedure;
+                //пиздец
+                command.Parameters.Add(new SqlParameter("@EmId", obj.Id));
+                command.Parameters.Add(new SqlParameter("@Birthday", obj.Birthday));
+                command.Parameters.Add(new SqlParameter("@Name", obj.Name));
+                command.Parameters.Add(new SqlParameter("@Surname", obj.Surname));
+                command.Parameters.Add(new SqlParameter("@Patronymic", obj.Patronymic));
+                command.Parameters.Add(new SqlParameter("@PositionId", obj.Position.Id));
+                command.Parameters.Add(new SqlParameter("@WorkingUnitId", obj.Unit.Id));
+                command.Parameters.Add(new SqlParameter("@CityIdOfRegistration", obj.CityOfRegistration.Id));
+                command.Parameters.Add(new SqlParameter("@CityIdOfResidence", obj.CityOfResidence.Id));
+                command.Parameters.Add(new SqlParameter("@EducationId", obj.Education.Id));
+                command.Parameters.Add(new SqlParameter("@ChildrenCount", obj.ChildrenCount));
+                command.Parameters.Add(new SqlParameter("@IsMarried", obj.IsMarried));
+                command.Parameters.Add(new SqlParameter("@PaspWho", obj.PaspWho));
+                command.Parameters.Add(new SqlParameter("@PassportCode", obj.PassportCode));
+                command.Parameters.Add(new SqlParameter("@PassportDate", obj.PassportDate));
+                command.Parameters.Add(new SqlParameter("@PassportNumber", obj.PassportNumber));
+                command.Parameters.Add(new SqlParameter("@PassportSeria", obj.PassportSeria));
+                command.Parameters.Add(new SqlParameter("@PlaceOfRegistration", obj.PlaceOfRegistration));
+                command.Parameters.Add(new SqlParameter("@PlaceOfResidence", obj.PlaceOfResidence));
+
+                _ = command.ExecuteNonQuery();
+            }
+        }
+
+        public static void UpdateUser(User obj)
+        {
+            const string sqlExpression = "UpdateUser";
+
+            using(SqlConnection connection = ConnectionFactory.GetSqlConnection())
+            {
+                SqlCommand command = new SqlCommand(sqlExpression, connection);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.Add(new SqlParameter("@UsId", obj.Id));
+                command.Parameters.Add(new SqlParameter("@Login", obj.Login));
+                command.Parameters.Add(new SqlParameter("@Password", obj.Password));
+                command.Parameters.Add(new SqlParameter("@Permissions", obj.Permissions));
+                command.Parameters.Add(new SqlParameter("@EmId", obj.Employee.Id));
+
+                _ = command.ExecuteNonQuery();
+            }
+        }
     }
 }
